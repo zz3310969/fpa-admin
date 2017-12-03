2e3a3f3cbff1f7739359386860985e1076645d67
lhttanner <lht_ge@163.com>
2017-12-03 15:22:34 +0800

    性格色彩

--- src/services/share.js
+++ src/services/share.js
@@ -2,7 +2,7 @@ import { stringify } from 'qs';
 import request from '../utils/request';
 
 export async function queryShare(params) {
-  return request(`/api/fpa/share?${stringify(params)}`);
+  return request(`/api/fpa/sharevo?${stringify(params)}`);
 }
 
 export async function addShare(params) {
